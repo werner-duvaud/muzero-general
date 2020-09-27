ab3ffe61dd2e687e1462594effa2d654eb0ae1b2
SanjoSolutions <61918211+SanjoSolutions@users.noreply.github.com>
2020-09-27 17:51:25 +0200

    Renames window_size to replay_buffer_size (#83)

--- games/atari.py
+++ games/atari.py
@@ -98,7 +98,7 @@ class MuZeroConfig:
 
 
         ### Replay Buffer
-        self.window_size = int(1e6)  # Number of self-play games to keep in the replay buffer
+        self.replay_buffer_size = int(1e6)  # Number of self-play games to keep in the replay buffer
         self.num_unroll_steps = 5  # Number of game moves to keep for every batch element
         self.td_steps = 10  # Number of steps in the future to take into account for calculating the target value
         self.PER = True  # Prioritized Replay (See paper appendix Training), select in priority the elements in the replay buffer which are unexpected for the network
