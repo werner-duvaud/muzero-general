798e4102b02e7484ca0275ef743f1c6347071dec
Tom White <tom@sixdozen.com>
2021-01-03 16:50:56 +1300

    Added submenu for simpler loading of pre-trained models
    Added a small sub-routine that scans for previously
    saved models. These can be loaded by simply selecting
    the menu option instead of providing the full path.
    The menu is presented in reverse chronological order
    and the final option "Specify paths manually" preserves
    the ability to provide a full path for the checkpoint
    and replay buffer as was done previously.

--- muzero.py
+++ muzero.py
@@ -5,6 +5,7 @@ import os
 import pickle
 import sys
 import time
+from glob import glob
 
 import nevergrad
 import numpy
@@ -560,6 +561,42 @@ def hyperparameter_search(
         text_file.close()
     return recommendation.value
 
+def load_model_menu(muzero, game_name):
+    # Configure running options
+    options = ["Specify paths manually"] + sorted(glob(f"results/{game_name}/*/"))
+    options.reverse()
+    print()
+    for i in range(len(options)):
+        print(f"{i}. {options[i]}")
+
+    choice = input("Enter a number to choose a model to load: ")
+    valid_inputs = [str(i) for i in range(len(options))]
+    while choice not in valid_inputs:
+        choice = input("Invalid input, enter a number listed above: ")
+    choice = int(choice)
+
+    if choice == (len(options) - 1):
+        # manual path option
+        checkpoint_path = input(
+            "Enter a path to the model.checkpoint, or ENTER if none: "
+        )
+        while checkpoint_path and not os.path.isfile(checkpoint_path):
+            checkpoint_path = input("Invalid checkpoint path. Try again: ")
+        replay_buffer_path = input(
+            "Enter a path to the replay_buffer.pkl, or ENTER if none: "
+        )
+        while replay_buffer_path and not os.path.isfile(replay_buffer_path):
+            replay_buffer_path = input(
+                "Invalid replay buffer path. Try again: "
+            )
+    else:
+        checkpoint_path = f"{options[choice]}model.checkpoint"
+        replay_buffer_path = f"{options[choice]}replay_buffer.pkl"
+
+    muzero.load_model(
+        checkpoint_path=checkpoint_path,
+        replay_buffer_path=replay_buffer_path,
+    )
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
@@ -612,22 +649,7 @@ if __name__ == "__main__":
             if choice == 0:
                 muzero.train()
             elif choice == 1:
-                checkpoint_path = input(
-                    "Enter a path to the model.checkpoint, or ENTER if none: "
-                )
-                while checkpoint_path and not os.path.isfile(checkpoint_path):
-                    checkpoint_path = input("Invalid checkpoint path. Try again: ")
-                replay_buffer_path = input(
-                    "Enter a path to the replay_buffer.pkl, or ENTER if none: "
-                )
-                while replay_buffer_path and not os.path.isfile(replay_buffer_path):
-                    replay_buffer_path = input(
-                        "Invalid replay buffer path. Try again: "
-                    )
-                muzero.load_model(
-                    checkpoint_path=checkpoint_path,
-                    replay_buffer_path=replay_buffer_path,
-                )
+                load_model_menu(muzero, game_name)
             elif choice == 2:
                 muzero.diagnose_model(30)
             elif choice == 3:
