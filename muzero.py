a4bf9a736ad51e5407c583ea921d207d72b8b8fd
Tom White <tom@sixdozen.com>
2021-01-08 06:22:57 +1300

    Change spaces to underscores to fix SummaryWriter error messages (#104)
    Updated strings passted to SummaryWriter.add_scalar removing
    spaces and replacing with underscores. Fixes #92.

--- muzero.py
+++ muzero.py
@@ -262,58 +262,58 @@ class MuZero:
             while info["training_step"] < self.config.training_steps:
                 info = ray.get(self.shared_storage_worker.get_info.remote(keys))
                 writer.add_scalar(
-                    "1.Total reward/1.Total reward",
+                    "1.Total_reward/1.Total_reward",
                     info["total_reward"],
                     counter,
                 )
                 writer.add_scalar(
-                    "1.Total reward/2.Mean value",
+                    "1.Total_reward/2.Mean_value",
                     info["mean_value"],
                     counter,
                 )
                 writer.add_scalar(
-                    "1.Total reward/3.Episode length",
+                    "1.Total_reward/3.Episode_length",
                     info["episode_length"],
                     counter,
                 )
                 writer.add_scalar(
-                    "1.Total reward/4.MuZero reward",
+                    "1.Total_reward/4.MuZero_reward",
                     info["muzero_reward"],
                     counter,
                 )
                 writer.add_scalar(
-                    "1.Total reward/5.Opponent reward",
+                    "1.Total_reward/5.Opponent_reward",
                     info["opponent_reward"],
                     counter,
                 )
                 writer.add_scalar(
-                    "2.Workers/1.Self played games",
+                    "2.Workers/1.Self_played_games",
                     info["num_played_games"],
                     counter,
                 )
                 writer.add_scalar(
-                    "2.Workers/2.Training steps", info["training_step"], counter
+                    "2.Workers/2.Training_steps", info["training_step"], counter
                 )
                 writer.add_scalar(
-                    "2.Workers/3.Self played steps", info["num_played_steps"], counter
+                    "2.Workers/3.Self_played_steps", info["num_played_steps"], counter
                 )
                 writer.add_scalar(
-                    "2.Workers/4.Reanalysed games",
+                    "2.Workers/4.Reanalysed_games",
                     info["num_reanalysed_games"],
                     counter,
                 )
                 writer.add_scalar(
-                    "2.Workers/5.Training steps per self played step ratio",
+                    "2.Workers/5.Training_steps_per_self_played_step_ratio",
                     info["training_step"] / max(1, info["num_played_steps"]),
                     counter,
                 )
-                writer.add_scalar("2.Workers/6.Learning rate", info["lr"], counter)
+                writer.add_scalar("2.Workers/6.Learning_rate", info["lr"], counter)
                 writer.add_scalar(
-                    "3.Loss/1.Total weighted loss", info["total_loss"], counter
+                    "3.Loss/1.Total_weighted_loss", info["total_loss"], counter
                 )
-                writer.add_scalar("3.Loss/Value loss", info["value_loss"], counter)
-                writer.add_scalar("3.Loss/Reward loss", info["reward_loss"], counter)
-                writer.add_scalar("3.Loss/Policy loss", info["policy_loss"], counter)
+                writer.add_scalar("3.Loss/Value_loss", info["value_loss"], counter)
+                writer.add_scalar("3.Loss/Reward_loss", info["reward_loss"], counter)
+                writer.add_scalar("3.Loss/Policy_loss", info["policy_loss"], counter)
                 print(
                     f'Last test reward: {info["total_reward"]:.2f}. Training step: {info["training_step"]}/{self.config.training_steps}. Played games: {info["num_played_games"]}. Loss: {info["total_loss"]:.2f}',
                     end="\r",
