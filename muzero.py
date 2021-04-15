f9f8ecd788d213dec59bdf841a4c95e7a7004ad4
Erik Gafni <egafni@gmail.com>
2021-04-15 22:52:08 +0300

    formatting

--- muzero.py
+++ muzero.py
@@ -88,7 +88,7 @@ class MuZero:
         if 1 < self.num_gpus:
             self.num_gpus = math.floor(self.num_gpus)
 
-        ray.init(num_gpus=total_gpus, ignore_reinit_error=True, )
+        ray.init(num_gpus=total_gpus, ignore_reinit_error=True)
 
         # Checkpoint and replay buffer used to initialize workers
         self.checkpoint = {
@@ -376,7 +376,7 @@ class MuZero:
             results.append(
                 ray.get(
                     self_play_worker.play_game.remote(
-                        0, 0, render, opponent, muzero_player
+                        0, 0, render, opponent, muzero_player,
                     )
                 )
             )
